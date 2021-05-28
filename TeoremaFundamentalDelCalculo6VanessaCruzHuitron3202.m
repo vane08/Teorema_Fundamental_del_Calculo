baf4e838a3f1d4234a735d1b0eca8885f57991e7
vane08 <82453121+vane08@users.noreply.github.com>
2021-05-28 17:19:18 -0500

    Delete TeoremaFundamentalDelCalculo6VanessaCruzHuitron3202.m

--- TeoremaFundamentalDelCalculo6VanessaCruzHuitron3202.m
+++
@@ -1,1 +0,0 @@
-%Titulo: Teorema fundamental del calculo(6)%Nombre:Vanessa Cruz Huitrón %Grupo:3202%Fecha: Jueves 27 de Mayo del 2021pkg load symbolicclear clcsyms x;syms y;f=(y^2)/(y+2)F=inline (char(f));a=-1b=1i=int(f,y)disp (" Teorema fundamental del calculo 6");F=int(f,y,a,b)
