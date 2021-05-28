e7584cb71b49c4470507de72f93a0917316d86b2
vane08 <82453121+vane08@users.noreply.github.com>
2021-05-28 13:05:56 -0500

    Add files via upload

---
+++ TeoremaFundamentalDelCalculo6VanessaCruzHuitron3202.m
@@ -0,0 +1,1 @@
+%Titulo: Teorema fundamental del calculo(6)%Nombre:Vanessa Cruz Huitrón %Grupo:3202%Fecha: Jueves 27 de Mayo del 2021pkg load symbolicclear clcsyms x;syms y;f=(y^2)/(y+2)F=inline (char(f));a=-1b=1i=int(f,y)disp (" Teorema fundamental del calculo 6");F=int(f,y,a,b)
